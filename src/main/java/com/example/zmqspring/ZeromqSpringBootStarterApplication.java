a45ede94502955ce2e12120e6650f945460f8e60
Artur Niemczyk <artekpol@wp.pl>
2018-04-08 13:45:50 +0200

    fix

--- src/main/java/com/example/zmqspring/ZeromqSpringBootStarterApplication.java
+++ src/main/java/com/example/zmqspring/ZeromqSpringBootStarterApplication.java
@@ -3,15 +3,9 @@ package com.example.zmqspring;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class ZeromqSpringBootStarterApplication extends SpringBootServletInitializer {
-
-    @Override
-    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-        return application.sources(ZeromqSpringBootStarterApplication.class);
-    }
+public class ZeromqSpringBootStarterApplication extends SpringApplicationBuilder {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ZeromqSpringBootStarterApplication.class, args);
